129e7e00da68f84c298104e3b8faf678ff7f08d1
Wutipat Khamnuansin <bellmcp@gmail.com>
2021-03-24 16:54:28 +0700

    Function

--- main.go
+++ main.go
@@ -2,32 +2,22 @@ package main
 
 import "fmt"
 
+// func add(a int, b int) int { // outer int = type of returned value
+// 	return a + b
+// }
+
+func add(a, b int) int { // grouped inner int
+	return a + b
+}
+
+func next(num int) (int, int) { // return more than one value
+	return num + 1, num + 2
+}
+
 func main() {
-	// day := "Sunday"
+	// result := add(10, 20)
+	// fmt.Println(result)
 
-	// switch day { // Case by case -> cannot group 2 case like other languages
-	// case "Sunday":
-	// 	fmt.Println("Weekend") // Auto break -> no need to break
-	// case "Saturday":
-	// 	fmt.Println("Weekend")
-	// default:
-	// 	fmt.Println("Workday")
-	// }
-
-	// // GROUPED CONDITION
-	// switch day {
-	// case "Sunday", "Saturday":
-	// 	fmt.Println("Weekend")
-	// default:
-	// 	fmt.Println("Workday")
-	// }
-
-	// DECLARE AND CONDITION
-
-	switch day := "Sunday"; day { // use ;
-	case "Sunday", "Saturday":
-		fmt.Println("Weekend")
-	default:
-		fmt.Println("Workday")
-	}
+	x, y := next(10) // x = 11, y =12
+	fmt.Println(x, y)
 }
