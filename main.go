d7e23435074dda8abf45cba23b52904a1108fc06
Wutipat Khamnuansin <bellmcp@gmail.com>
2021-03-29 15:12:36 +0700

    Make

--- main.go
+++ main.go
@@ -3,21 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	words := []string{"Hello", "Hi", "Bye", "Wutipat", "Bell"}
+	// s1 := []int{1, 2, 3, 4, 5}
+	// fmt.Println(s1, len(s1), cap(s1))
 
-	// FIND
-	fmt.Println(words[2])
+	// MAKE
+	s2 := make([]int, 0, 5) //length = 1, capacity = 5
+	s2 = append(s2, 1, 2)
 
-	// APPEND
-	words = append(words, "Go")
-	fmt.Println(words)
+	s3 := append(s2, 6)
+	s3[0] = 10 // This will change both s2[0] and s3[0] to 10 because it shared the same array
 
-	// REMOVE -> "Bye"
-	// {"Hello", "Hi"} + {"Wutipat", "Bell"}
-	// words = append(words[:2], words[3:]) //start with index 0 to 1 (except words[2]) then merges with the rest
-	// ** CANNOT DO THIS -> it cannot concat 2 slices
+	// BUT IF WE DO THIS
+	// if s2 := make([]int, 0, 5)
+	// s2 = append(s2, 1, 2, 3, 4, 5)
+	// s3 := append(s2, 6)
+	// this will make s2 = [1 2 3 4 5] and s3 = [1 2 3 4 5 6] which are different array because the s2 is already full (due to capacity of 5)
 
-	// DO THIS
-	words = append(words[:2], words[3:]...) // ... behind slices is like spread operator in JS
-	fmt.Println(words)
+	// SO, IF WE DO
+	// s3[0] = 10 -> This will change only s3[0] to 10
+
+	fmt.Println(s2, s3)
 }
